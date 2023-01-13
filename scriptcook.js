a96cfcdcfe105d96aee50623ae5c05b630f79b8b
Abdelbaki <itshubie@hotmail.com>
2023-01-13 16:06:01 +0100

    fonction surprise hidden btn

--- scriptcook.js
+++ scriptcook.js
@@ -8,30 +8,45 @@ const updatescore = (cookies) => {
 let clicks = 0;
 let doubleScore = 1;
 let newclick = 1;
+let today = new Date();
 const hidden = document.querySelector(".hidden");
+
 function MuchCoockie() {
   clicks += 1;
-
-  document.getElementById("score").innerHTML = clicks + " : Coockies";
+  let Seconds = today.getMilliseconds();
+  document.getElementById("score").innerHTML =
+    clicks + " : Coockies<br/>" + Seconds + "  times games";
   hidden.style.visibility = "hidden";
-  document.getElementById("doublesc").disabled=false;
-  if (clicks % 20 == 0) {
+  document.getElementById("doublesc").disabled = true;
+  if (clicks % 25 == 0) {
     alert("click for doublescore !!");
     hidden.style.visibility = "visible";
+    document.getElementById("doublesc").disabled = false;
   }
-
-  if (clicks == 100) {
+  if (clicks == 165) {
     hidden.style.visibility = "visible";
-    alert("ourahhhhhhhhhhhhhhhhhhhhhhhhhhhhhh coockies explosion!!!!");
-    clicks = 0;
+    alert("ourahhhhhhhhhhhhhhhhhhhhhhhhhhhhhh coockies explosion!!!! ");
   }
 }
-function DoubleScore() {
-  if (clicks % 20 == 0) {
+function Double() {
+  if (clicks % 25 == 0) {
     doubleScore = clicks * 2;
     clicks = doubleScore;
-
-    document.getElementById("score").innerHTML = doubleScore + " : Coockies";
-    document.getElementById("doublesc").disabled=true;
+    document.getElementById("score").innerHTML =
+      doubleScore + " : Coockies<br/>";
+    document.getElementById("doublesc").disabled = true;
+  } else if (clicks == 150) {
+    document.getElementById("score").innerHTML =
+      clicks + " : Coockies soon FINITO !!!!";
+  } else {
+    clicks = 0;
+    document.getElementById("score").innerHTML =
+      clicks + " : Coockies so FINITO !!!!";
   }
+}
+
+function BonusHidden() {
+  let newbtn = document.querySelector(".hidden");
+
+  newbtn.style.backgroundColor = "green";
 }
